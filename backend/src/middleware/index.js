89a8ae20dc04a61e3f6e8a49a72c01882d996a1b
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2023-10-17 14:12:36 +0700

    feat: export all middlewares

---
+++ backend/src/middleware/index.js
@@ -0,0 +1,6 @@
+import { verifyAdmin } from './admin';
+import { verifyAuth } from './auth';
+import { verifyTenant } from './tenant';
+import { verifyOwner } from './owner';
+
+export { verifyAdmin, verifyAuth, verifyTenant, verifyOwner };

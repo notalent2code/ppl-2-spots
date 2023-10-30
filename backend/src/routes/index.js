4633b37436c8a722137214a23f6a08183e56207b
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2023-10-30 21:27:02 +0700

    feat: create index routes

---
+++ backend/src/routes/index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import { verifyAuth } from '../middleware';
+
+import adminRouter from './admin';
+import tenantRouter from './tenant';
+import ownerRouter from './owner';
+import authRouter from './auth';
+import bookingRouter from './booking';
+import coworkingSpaceRouter from './coworkingSpace';
+import paymentRouter from './payment';
+
+const indexRouter = express.Router();
+
+indexRouter.get('/', (req, res, next) => {
+  res.status(200).json({ message: 'Welcome to Spots API' });
+});
+
+indexRouter.get('/test-auth', verifyAuth, (req, res, next) => {
+  res.status(200).json({ message: 'Authentication testing success' });
+});
+
+export {
+  indexRouter,
+  authRouter,
+  adminRouter,
+  tenantRouter,
+  ownerRouter,
+  bookingRouter,
+  coworkingSpaceRouter,
+  paymentRouter,
+};

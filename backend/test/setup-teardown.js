6e79a7dd90959278eb06440484368bcd1896ab1b
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2023-11-09 19:43:37 +0700

    test: Add server variable and teardown function to test setup

--- backend/test/setup-teardown.js
+++ backend/test/setup-teardown.js
@@ -1,17 +1,19 @@
-import app from '../src/app';
 import pactum from 'pactum';
+import app from '../src/app';
+
+let server;
 
 export const setupTestEnivironment = async () => {
   const port = 5555;
 
-  app.listen(port, () => {
+  server = app.listen(port, () => {
     console.log(`Testing server is running on port ${port}`);
   });
 
   pactum.request.setDefaultTimeout(10000);
   pactum.request.setBaseUrl(`http://localhost:${port}`);
-}
+};
 
-// export const teardownTestEnvironment = async () => {
-  
-// }
+export const teardownTestEnvironment = async () => {
+  server.close();
+};

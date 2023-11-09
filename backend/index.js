61ed610150cf57791ebc791c248ab47f35292b3f
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2023-11-09 19:46:01 +0700

    chore: Add constants file and log NODE_ENV in server startup message

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 import app from './src/app';
+import constants from './src/config/constants';
 
 function bootstrap(app, port) {
   app.listen(port, () => {
+    console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
     console.log(`Server is running on http://localhost:${port}`);
   });
 }
